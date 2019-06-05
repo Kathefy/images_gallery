aef6a8e9ce52f2ae81370fbfa6d939a42cfc4d1b
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-06-05 22:32:14 +0200

    Show images from catalog using import

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-console.log('Hello World');
+// Wyświetlanie zdjęć z katalogu images - tablice, pętle
+import image1 from '/images/img1.jpg';
+import image2 from '/images/img2.jpg';
+import image3 from '/images/img3.jpg';
+import image4 from '/images/img4.jpg';
+import image5 from '/images/img5.jpg';
+
+const imagesPaths = [image1, image2, image3, image4, image5];
+
+const gallery = document.getElementById('images');
+
+imagesPaths.forEach(path => {
+  let image = document.createElement('img');
+  image.setAttribute('src', path);
+  image.className = 'images__image';
+
+  gallery.appendChild(image);
+});
+
+console.log('hello');
