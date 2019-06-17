853d8ce11e6866e5b4db4d168c18c8e88a201541
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-06-17 22:22:23 +0200

    Filter images

--- js/app.js
+++ js/app.js
@@ -11,17 +11,29 @@ const imagesPaths = [image1, image2, image3, image4, image5];
 const images = imagesPaths.map(path => new Image(path));
 images.forEach(image => image.showImage());
 
+// Filtrowanie zdjęć
 const btnShowFavorite = document.getElementById('showFavourites');
+let showAll = false;
 btnShowFavorite.addEventListener('click', () => {
-  images.filter(image => !image.showFavorite()).forEach(image => image.hide());
+  if (showAll) {
+    images.forEach(image => image.show());
+    btnShowFavorite.innerText = 'Show favourites';
+    showAll = false;
+  } else {
+    images
+      .filter(image => !image.showFavorite())
+      .forEach(image => image.hide());
+    btnShowFavorite.innerText = 'Show all';
+    showAll = true;
+  }
 });
 
-const btnShowAll = document.getElementById('showAll');
-btnShowAll.addEventListener('click', () => {
-  images.forEach(image => image.show());
-});
+// const btnShowAll = document.getElementById('showAll');
+// btnShowAll.addEventListener('click', () => {
+//   images.forEach(image => image.show());
+// });
 
-//dodawanie zdjęcia z komputera - File API
+// Dodawanie zdjęcia z komputera - File API
 const inputUpload = document.getElementById('file-input');
 inputUpload.onchange = () => {
   if (inputUpload.files && inputUpload.files[0]) {
