c13ce220571b62fba3fcc5c361a3e014eb9984ab
Katsiaryna Fedarovich <kathefy215@gmail.com>
2019-06-17 21:58:18 +0200

    Add image from pc, file API

--- js/app.js
+++ js/app.js
@@ -20,3 +20,19 @@ const btnShowAll = document.getElementById('showAll');
 btnShowAll.addEventListener('click', () => {
   images.forEach(image => image.show());
 });
+
+//dodawanie zdjęcia z komputera - File API
+const inputUpload = document.getElementById('file-input');
+inputUpload.onchange = () => {
+  if (inputUpload.files && inputUpload.files[0]) {
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      const image = new Image(e.target.result);
+      image.showImage();
+      images.push(image);
+    };
+
+    reader.readAsDataURL(inputUpload.files[0]);
+  }
+};
